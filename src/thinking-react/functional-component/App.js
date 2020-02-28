6b486b6336c731a0d192cf07784ec4914f3f9152
SepSol <SR-75@live.com>
2020-02-28 22:19:10 +0330

    minor fixes

--- src/thinking-react/functional-component/App.js
+++ src/thinking-react/functional-component/App.js
@@ -1,11 +1,5 @@
-{category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-{category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-{category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-{category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-{category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-{category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+// import ReactDOM from 'react-dom';
 
 
 
@@ -151,9 +145,10 @@ function FilterableProductTable({products}) {
 
 
 
-export default FilterableProductTable;
 
 // ReactDOM.render(
-//   <FilterableProductTable products={PRODUCTS} />,
-//   document.getElementById('root')
-// );
+    //   <FilterableProductTable products={PRODUCTS} />,
+    //   document.getElementById('root')
+    // );
+    
+export default FilterableProductTable;
